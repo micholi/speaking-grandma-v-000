906de8b49f4e13fc6dc814c2b6769a1c6c97ffd6
Michelle Olivieri <michelleo72@gmail.com>
2018-03-19 21:24:13 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
 
-  elsif != phrase.upcase
+  elsif phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
 
   elsif phrase == phrase.upcase
