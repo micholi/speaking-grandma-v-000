1509c59fffcc4b68fafc97871143db3dd28c406f
Michelle Olivieri <michelleo72@gmail.com>
2018-03-19 21:18:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,13 +13,13 @@ def speak_to_grandma(phrase)
 
 # NO, NOT SINCE 1938!
 
-  elsif phrase == phrase.upcase
+  if phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
 
   end
